812fa0fedd4dab803f7e428b4ab12df018e67deb
qaaid <qaaid@medoment.com>
2026-07-20 21:12:54 +0530

    r

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,22 +2,17 @@ import { PrismaClient } from "@prisma/client";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not set");
-}
-
-const pool = new Pool({
-  connectionString,
-});
-
-const adapter = new PrismaPg(pool);
-
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    adapter,
-  });
+  const connectionString = process.env.DATABASE_URL;
+
+  if (!connectionString) {
+    throw new Error("DATABASE_URL is not set");
+  }
+
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaPg(pool);
+
+  return new PrismaClient({ adapter });
 };
 
 declare global {
